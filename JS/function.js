ed16c7eb0f7197b0d2fe2d8ffe30dbc53545c26e
IJouu <ijou86218@gmail.com>
2019-01-25 02:40:02 +0800

    UI

--- JS/function.js
+++ JS/function.js
@@ -1,14 +1,10 @@
 $(document).ready(function () {
   M.AutoInit();
-  $(document).change(function () {
-    var all = $("#test").serializeArray();
-    console.log(all);
-  });
 
   function getData() {
     $(`#table-content`).html(`<h4 class="grey-text text-darken-2">載入中...</h4>`);
     $.ajax({
-      url: `https://testsite.staging.seekrtech.com/forest_rankings?n=10&last_pos=0`,
+      url: `https://testsite.staging.seekrtech.com/forest_rankings?n=19&last_pos=0`,
       type: "GET",
       success: function (result) {
         console.log(result);
@@ -24,33 +20,52 @@ $(document).ready(function () {
 
   function render(result) {
     var str = "";
-    // var photo = "";
-    for (const key in result) {
-      const element = result[key];
-      for (const inner_key in element) {
-        const inner_element = element[inner_key];
-        if (inner_element.avatar != null) {
-          str += `
+    var ranking = 1;
+    const element = result["ranking"];
+    for (const inner_key in element) {
+      const inner_element = element[inner_key];
+      if (inner_element.avatar != null) {
+        str += `
+        <div id="badge">
+          <img src="./icon/leaderboard_badge.png" >
+          <h id="ranking">${ranking}</h>
+        </div>
+        <div class="card">
+        <table>
             <tr>
-              <td><img src="${inner_element.avatar}"><td>
-              <td>${inner_element.name}</td>
-              <td>${inner_element.health_count}</td>
-              <td>${inner_element.dead_count}</td>
-              <td>${inner_element.total_minutes}</td>
+              <td rowspan="2" class="td-align"><img src="${inner_element.avatar}"></td>
+              <td colspan="3">${inner_element.name}</td>
             </tr>
-        `;
-        } else {
-          str += `
             <tr>
-              <td><img src="https://robohash.org/309125.jpg?size=200x200\u0026set=set1"><td>
-              <td>${inner_element.name}</td>
-              <td>${inner_element.health_count}</td>
-              <td>${inner_element.dead_count}</td>
-              <td>${inner_element.total_minutes}</td>
+              <td><img src="./icon/tree_icon_healthy.png" class="icon">&nbsp${inner_element.health_count}</td>
+              <td><img src="./icon/tree_icon_dead.png" class="icon">&nbsp${inner_element.dead_count}</td>
+              <td>${inner_element.total_minutes}分鐘</td>
             </tr>
+        </table>
+        </div>
         `;
-        }
+      } else {
+        str += `
+        <div id="badge">
+          <img src="./icon/leaderboard_badge.png" >
+          <h id="ranking">${ranking}</h>
+        </div>
+        <div class="card">
+        <table>
+            <tr>
+              <td rowspan="2" class="td-align"><img src="./icon/icon_120.png"></td>
+              <td colspan="3">${inner_element.name}</td>
+            </tr>
+            <tr>
+            <td><img src="./icon/tree_icon_healthy.png" class="icon">&nbsp${inner_element.health_count}</td>
+            <td><img src="./icon/tree_icon_dead.png" class="icon">&nbsp${inner_element.dead_count}</td>
+              <td>${inner_element.total_minutes}分鐘</td>
+            </tr>
+        </table>
+        </div>
+        `;
       }
+      ranking += 1;
     }
     $(`#table-content`).html(str);
   }
